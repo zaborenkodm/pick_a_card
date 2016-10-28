36e35eb72354bd84e6eb3baeae9708d3fc75d1e3
zaborenkodm <zaborenkodm@gmail.com>
2016-10-28 16:12:08 +0300

    добавил значки карт вместо их названий

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,18 @@
 # encoding: utf-8
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # ã¡¨,—¥à¢¨, Šà¥áâ¨, ¨ª¨
+# Программа, которая выбирает произвольную карту игральной колоды
 
-puts "#{values.sample} of #{suits.sample}"
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+# Массивы с наборами мастей и достоинств
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+suits = ['♦','♠','♥ ','♣']
+
+# Выведем произвольную карты, выбрав по одному элементу из массивов
+puts "#{values.sample}#{suits.sample}"
