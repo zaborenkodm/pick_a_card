c483051cc5d60ec5dabb66c7ed126043e21a95a9
zaborenkodm <zaborenkodm@gmail.com>
2016-10-28 15:10:30 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
-
+# encoding: utf-8
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # γ΅¨,—¥ΰΆ¨, ΰ¥αβ¨, ¨ª¨
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # ΒΓ£Β΅Β¨,β€”Β¥Γ ΒΆΒ¨, Ε Γ Β¥Γ΅ΓΆΒ¨, ΒΒ¨ΒªΒ¨
 
 puts "#{values.sample} of #{suits.sample}"
